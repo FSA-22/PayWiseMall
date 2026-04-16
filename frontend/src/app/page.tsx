7e7e6348674ab90b515c9ef688a098ef7d30aee5
FSA-22 <simeonstat@gmail.com>
2026-04-16 21:07:07 +0000

    feat: set up basic global css file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex-center font-extrabold title-text text-primary-1 h-screen w-full">
+      Home
+    </div>
+  );
 };
 
 export default Home;
